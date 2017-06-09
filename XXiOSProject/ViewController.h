b9370c8f76713ee37226ddb74593c0c998bcdf2e
linzhipei <beelin9527@foxmail.com>
2017-06-09 15:13:54 +0800

    Initial Commit

---
+++ XXiOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXiOSProject
+//
+//  Created by Beelin on 17/6/9.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XXiOSProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XXiOSProject
+//
+//  Created by Beelin on 17/6/9.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
