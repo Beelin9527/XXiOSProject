0330e43bd0f89a425c2994ecc553f45018f014c8
linzhipei <beelin9527@foxmail.com>
2017-06-14 14:57:29 +0800

    update Global dir

---
+++ XXiOSProject/Utils/Global/XXConst.h
@@ -0,0 +1,13 @@
+//
+//  XXConst.h
+//  XXiOSProject
+//
+//  Created by Beelin on 17/6/14.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXConst : NSObject
+
+@end
---
+++ XXiOSProject/Utils/Global/XXMacros.h
@@ -0,0 +1,13 @@
+//
+//  XXMacros.h
+//  XXiOSProject
+//
+//  Created by Beelin on 17/6/14.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#ifndef XXMacros_h
+#define XXMacros_h
+
+
+#endif /* XXMacros_h */
